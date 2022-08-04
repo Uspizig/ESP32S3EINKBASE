50aa1e138933e2f4b1ca6de7f8893debb65fcfc3
Uspizig <56263335+Uspizig@users.noreply.github.com>
2022-08-04 20:37:21 +0200

    Add files via upload

---
+++ Software/einkTest/user_kennung.h
@@ -0,0 +1,24 @@
+#define debug false
+//#define debug true
+#define WLAN_SSID       "wallyweb2"  
+#define WLAN_PASS       "xxxxxxxxxx"
+//#define WLAN_SSID       "xxxxx"  //Put your SSID here
+//#define WLAN_PASS       "xxxxxx"  //Put you wifi password here
+
+
+//const unsigned long UpdateInterval = (40L * 60L) * 1000000L; //60sec mal 60 = 1h
+const unsigned long UpdateInterval = (30L * 60L) * 1000000L; //60sec mal 60 = 2min
+
+
+//Google
+static char hostname[] = "Muelli"; // That's the name you will see in your network.
+const char *GScriptId = "xxxxxxxxxxxxxxxxxxxx";
+
+#define WU_API_KEY "xxxxxxxxxx"
+//#define WU_API_KEY "xxxxxxxxxxxxxx"
+
+//Connection Settings
+const char* host = "script.google.com";
+const char* googleRedirHost = "script.googleusercontent.com";
+const char*  server = "script.google.com";
+const int httpsPort = 443;
